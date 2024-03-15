1471800574a1101ec6170b46d8954e95cda6b75a
Lemoine <lemoine.charlotte@gmail.com>
2024-03-15 11:17:21 +0100

    add router and json files

---
+++ src/pages/APropos/index.js
@@ -0,0 +1,9 @@
+function APropos() {
+  return (
+    <div className="APropos">
+      <p>PAGE DEUX </p>
+    </div>
+  );
+}
+
+export default APropos;
---
+++ src/pages/Accueil/index.js
@@ -0,0 +1,9 @@
+function Accueil() {
+  return (
+    <div className="Accueil">
+      <p>Hello Kasa </p>
+    </div>
+  );
+}
+
+export default Accueil;
